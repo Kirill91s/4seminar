231751d91a1138055076b40f4b23d532ce2fe481
Kirill91s <kirill2542@gmail.com>
2023-07-31 21:12:28 +0300

    4seminar

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿// Семинар 4
+// int sum (int a)
+// {
+//     int result = 0;
+//     for(int i = 1; i <= a; i++)
+//         result += i;
+//     return result;
+// }
+// Console.WriteLine("in put the number");
+
+// int nunumber = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(sum(nunumber));
+
+// 2 задача
+// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе. 
+// int CountNumbers(int N)
+// {
+//     int count = 1;
+//     for(; 10 <= N; count++)
+//         N = N/10;
+//     return count; 
+// }
+// Console.WriteLine("input the number");
+// int nunumber = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(CountNumbers(nunumber));
+
+
+// 3 задача
+// Напишите программу, которая принимает на вход число N и выдаёт произведение нечетных чисел от 1 до N.
+// int prov (int a)
+// {
+//     int result = 1;
+//     for(int i = 1; i <= a; i= i+2)
+//         result *= i;
+//     return result;
+// }
+// Console.WriteLine("in put the number");
+
+// int nunumber = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(prov(nunumber));
